29cf6fe2e8f665687e1bba908498afdd0d436362
Taichi Sato <okms310gmail.com>
2017-11-15 22:53:21 +0900

    FirstCommit

---
+++ Assets/BackTracking/BackTracking.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackTracking : MonoBehaviour {
+
+	void Start () {
+		Debug.Log ("てれれれ~ ばっくとらっくほう~");
+	}
+}
