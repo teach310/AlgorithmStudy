03c308b53a8e9db28ec8d6da807c0737e5eeb976
Taichi Sato <okms310gmail.com>
2017-11-17 00:03:07 +0900

    テンプレを作成

---
+++ Assets/BackTracking/Kadai7Template.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text;
+using System.IO;
+using System.Linq;
+
+namespace Template
+{
+	public class DPSumSlow{
+		int[] nums;
+		int m;
+		int ans;
+
+		public int Solve(int[] nums, int m){
+			this.nums = nums;
+			this.m = m;
+			ans = int.MaxValue;
+			return (ans == int.MaxValue) ? -1 : ans;
+		}
+
+		void Solve(int level, int k, int sum){
+			// ここに書くべきコードを考えること．
+		}
+	}
+
+	public class Kadai7 : MonoBehaviour
+	{
+
+		public TextAsset textAsset;
+
+		void Start ()
+		{
+			Scanner sc = new Scanner (textAsset.text);
+			DPSumSlow sol = new DPSumSlow ();
+			while (true) {
+				int m = sc.NextInt ();
+				int n = sc.NextInt ();
+				if (m == 0)
+					break;
+				int[] a = new int[n];
+
+				for (int i = 0; i < n; i++)
+					a [i] = sc.NextInt ();
+				Debug.Log (sol.Solve (a, m));
+			}
+		}
+	}
+}
---
+++ Assets/BackTracking/Teach/BackTracking.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Teach
+{
+	public class BackTracking
+	{
+		public void Do ()
+		{
+			Debug.Log ("てれれれ~ ばっくとらっくほう~");
+		}
+	}
+}
