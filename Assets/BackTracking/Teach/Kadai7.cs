972b703b663e696a59df0a8841487617dad1ff5d
Taichi Sato <okms310gmail.com>
2017-11-17 21:16:49 +0900

    分岐限定法

--- Assets/BackTracking/Teach/Kadai7.cs
+++ Assets/BackTracking/Teach/Kadai7.cs
@@ -8,19 +8,43 @@ using System.Linq;
 namespace Teach
 {
 	public class DPSumSlow{
-		int[] nums;
-		int m;
-		int ans;
+		int[] nums; // 棒の長さの配列
+		int m; // 目標値
+		int ans; // 総重量
+		public int count = 0;
 
 		public int Solve(int[] nums, int m){
 			this.nums = nums;
 			this.m = m;
 			ans = int.MaxValue;
+			Solve (0, 0, 0);
 			return (ans == int.MaxValue) ? -1 : ans;
 		}
 
+		/// <summary>
+		/// Solve the specified level, k and sum.
+		/// </summary>
+		/// <param name="level">Level.</param>
+		/// <param name="k">K.</param>
+		/// <param name="sum">Sum.</param>
 		void Solve(int level, int k, int sum){
 			// ここに書くべきコードを考えること．
+			// 最小値の更新
+			if (sum == m && k < ans)
+				ans = k;
+			
+			#region もうこれ以上計算しなくていい条件
+			if (level >= nums.Length)
+				return;
+			if (sum >= m)
+				return;
+			if (k >= ans)
+				return;
+			#endregion
+
+
+			Solve (level + 1, k + 1, sum + nums [level]);
+			Solve (level + 1, k, sum);
 		}
 	}
 
